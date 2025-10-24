214efc9d06ccc65aa533bd04a73ca66a5051c1e2
salahmed-ctrlz <medkoursalaheddine@gmail.com>
2025-10-24 22:30:45 +0100

    Edits 1+

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/HamoudiBenarba",
+  base: "/HamoudiBenarba/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
